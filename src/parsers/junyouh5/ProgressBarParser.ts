7a581582f5370115aa7e9de6061e217793a01b52
pb24 <20357427@qq.com>
2016-06-28 15:49:50 +0800

    去掉图层命名限制

--- src/parsers/junyouh5/ProgressBarParser.ts
+++ src/parsers/junyouh5/ProgressBarParser.ts
@@ -1,3 +1,10 @@
+/**
+ * 进度条解析器
+ * 
+ * @class ProgressBarParser
+ * @extends {ComWillCheck}
+ * @author pb
+ */
 class ProgressBarParser extends ComWillCheck {
     constructor() {
         super(ExportType.ProgressBar, /^ui[.](progress)/, null, "ProgressBar");
@@ -5,10 +12,13 @@ class ProgressBarParser extends ComWillCheck {
     }
     /**
      * 用于处理进度条
+     * 支持3图层 txt bar bg
+     * 支持2图层 txt bar
      */
     private progressBarParser(checker: ComWillCheck, item: FlashItem, list: any[], solution: Solution) {
         // 检查进度条的帧
         let timeline = item.timeline;
+        // 多图层
         let layers = timeline.layers;
         let len = layers.length;
         let data = [];
@@ -19,34 +29,27 @@ class ProgressBarParser extends ComWillCheck {
         let frame;
         let elements;
         let barWidth;
+        // 遍历图层
         for (let i = 0; i < len; i++) {
             layer = layers[i];
-            name = layer.name;
-
-            if (name === "tf") {
-                frame = layer.frames[0];
-                elements = frame.elements;
-                let tf = elements[0];
-                if (tf && tf.elementType === "text") {
-                    data[0] = solution.getElementData(tf);
-                }
-            } else if (name === "bar") {
-                frame = layer.frames[0];
-                elements = frame.elements;
-                let bar = elements[0];
-                let barData = solution.getElementData(bar);
-                data[1] = barData;
-                if (barData)
-                    barWidth = barData[1][3];
-                else
-                    Log.throwError("进度条宽度未设置，请确认.", item.name);
-            }
-            else if (name === "bg") {
-                frame = layer.frames[0];
-                elements = frame.elements;
-                let bg = elements[0];
-                if (bg.elementType === "instance" && bg.instanceType === "bitmap") {
-                    let bgData = solution.getElementData(bg);
+            frame = layer.frames[0];
+            elements = frame.elements;
+            let e = elements[0];
+            if (e) {
+                //文本
+                if (e.elementType === "text") {
+                    data[0] = solution.getElementData(e);
+                } // 进度条
+                else if (e.elementType === "instance" && e.instanceType === "symbol") {
+                    let barData = solution.getElementData(e);
+                    data[1] = barData;
+                    if (barData)
+                        barWidth = barData[1][3];
+                    else
+                        Log.throwError("进度条宽度未设置，请确认.", item.name);
+                } // 底图 无底图时不处理
+                else if (e.elementType === "instance" && e.instanceType === "bitmap") {
+                    let bgData = solution.getElementData(e);
                     data[2] = bgData;
                     if (bgData) {
                         let bgWidth = bgData[1][3];
