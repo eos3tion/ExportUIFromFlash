e46639d30499ce9e01cfb50832dcb4ddf9c6d830
顾帅 <gushuai@junyougame.com>
2016-08-18 15:59:49 +0800

    定位用的解析

--- src/generators/JunyouH5Generator.ts
+++ src/generators/JunyouH5Generator.ts
@@ -144,40 +144,58 @@ class JunyouH5Generator implements IPanelGenerator {
                     } else {
                         let tp = instanceName.split("$")[0];
                         let tmpname = instanceName.split("$")[1];
-                        pros.push("public " + tmpname + ":egret.Sprite;");
+                       
+                        
                         let tmpd = data[2][0];
+                         if(tp=="con"){
+                             if(tmpd){
+                                 pros.push("public " + tmpname + ":egret.Rectangle;");
+                                 comps.push("this."+tmpname+"=new egret.Rectangle();");
+                             }else{
+                                 pros.push("public " + tmpname + ":egret.Sprite;");
+                                 comps.push("this."+tmpname+"=new egret.Sprite();");
+                             }
+                        }else{
+                            pros.push("public " + tmpname + ":egret.Sprite;");
+                            comps.push("this."+tmpname+"=new egret.Sprite();");
+                        }
                         //tmpd[1][0]=tmpname;
-
-                        if (tmpd) {
-                            tmpd[1][1] = 0;
-                            tmpd[1][2] = 0;
-                        }
-
-
-                        comps.push("this." + tmpname + "=new egret.Sprite();");
-                        if (tp != "con") {
-                            if (tmpd) {
+       
+                            if(tmpd){
+                                tmpd[1][1]=0;
+                                tmpd[1][2]=0;
+                            }
+                        
+                        
+                        
+                        if(tp!="con"){
+                            if(tmpd){
                                 comps.push("dis=manager.createBitmapByData(this._key, " + JSON.stringify(tmpd) + ");");
-                                comps.push("this." + tmpname + ".addChild(dis);");
+                                comps.push("this."+tmpname+".addChild(dis);");
                             }
                         }
-
-                        if (data[1][1] != 0) {
-                            comps.push("this." + tmpname + ".x=" + data[1][1] + ";");
+                        
+                        if(data[1][1]!=0){
+                            comps.push("this."+tmpname+".x="+data[1][1]+";");
                         }
-                        if (data[1][2] != 0) {
-                            comps.push("this." + tmpname + ".y=" + data[1][2] + ";");
+                        if(data[1][2]!=0){
+                            comps.push("this."+tmpname+".y="+data[1][2]+";");
                         }
-                        if (tp == "con") {
-                            if (tmpd) {
-                                comps.push("this." + tmpname + ".graphics.clear();");
-                                comps.push("this." + tmpname + ".graphics.beginFill(0,0);");
-                                comps.push("this." + tmpname + ".graphics.drawRect(0,0," + data[1][3] + "," + data[1][4] + ");");
-                                comps.push("this." + tmpname + ".graphics.endFill();");
+                        if(tp=="con"){
+                            if(tmpd){
+                                comps.push("this."+tmpname+".width="+data[1][3]+";");
+                                comps.push("this."+tmpname+".height="+data[1][4]+";");
                             }
-
+                            
                         }
-                        comps.push("this.addChild(this." + tmpname + ");");
+                        if(tp=="con"){
+                            if(!tmpd){
+                                comps.push("this.addChild(this."+tmpname+");");
+                            }
+                            
+                        }else{
+                            comps.push("this.addChild(this."+tmpname+");");
+                        }
                     }
 
                     break;
