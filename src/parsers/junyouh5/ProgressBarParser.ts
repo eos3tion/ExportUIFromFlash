39c999ee102d5de0f88269cab453d7540a887f45
pb <pengbo@junyougame.com>
2016-10-17 14:45:53 +0800

    ProgressBar优化

--- src/parsers/junyouh5/ProgressBarParser.ts
+++ src/parsers/junyouh5/ProgressBarParser.ts
@@ -29,30 +29,34 @@ class ProgressBarParser extends ComWillCheck {
         let frame;
         let elements;
         let barWidth;
+        let bData;
         // 遍历图层
         for (let i = 0; i < len; i++) {
             layer = layers[i];
-            frame = layer.frames[0];
-            elements = frame.elements;
-            let e = elements[0];
-            if (e) {
+            name = layer.name;
+            if (name) {
+                frame = layer.frames[0];
+                elements = frame.elements;
+                let e = elements[0];
                 //文本
-                if (e.elementType === "text") {
+                if (name === "tf") {
                     data[0] = solution.getElementData(e);
-                } // 进度条
-                else if (e.elementType === "instance" && e.instanceType === "symbol") {
-                    let barData = solution.getElementData(e);
-                    data[1] = barData;
-                    if (barData)
-                        barWidth = barData[1][3];
+                }
+                // 进度条
+                else if (name === "bar") {
+                    bData = solution.getElementData(e);
+                    data[1] = bData;
+                    if (bData)
+                        barWidth = bData[1][3];
                     else
                         Log.throwError("进度条宽度未设置，请确认.", item.name);
-                } // 底图 无底图时不处理
-                else if (e.elementType === "instance" && e.instanceType === "bitmap") {
-                    let bgData = solution.getElementData(e);
-                    data[2] = bgData;
-                    if (bgData) {
-                        let bgWidth = bgData[1][3];
+                }
+                // 底图 无底图时不处理
+                else if (name === "bg") {
+                    bData = solution.getElementData(e);
+                    data[2] = bData;
+                    if (bData) {
+                        let bgWidth = bData[1][3];
                         if (barWidth && bgWidth && barWidth > bgWidth)
                             Log.throwError("进度条宽度大于底图宽度，请确认", item.name);
                     }
