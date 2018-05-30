50433e789044d73b3144432f03482af3a7a93555
程方 <chengfang@junyougame.com>
2018-05-30 17:11:57 +0800

    现在ScaleBitmap不再导出`ScaleBitmap`类型，直接使用`egret.Bitmap`

--- src/parsers/junyouh5/ScaleBitmapParser.ts
+++ src/parsers/junyouh5/ScaleBitmapParser.ts
@@ -1,6 +1,6 @@
 class ScaleBitmapParser extends ComWillCheck {
     constructor() {
-        super(ExportType.ScaleBmp, /^bmd[.](scale9)[.]/, null, "ScaleBitmap");
+        super(ExportType.ScaleBmp, /^bmd[.](scale9)[.]/, null, "egret.Bitmap");
     }
 
     doParser(item: FlashItem, solution: Solution) {
